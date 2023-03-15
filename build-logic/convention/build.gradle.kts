10f4e2ec0591fd2b7b2ef49756be6e55c23c0198
George <aptx113@gmail.com>
2023-03-15 12:11:02 +0800

    add libs.version.toml

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     `kotlin-dsl`
 }
+
+group = "com.dante.calorietracker.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
