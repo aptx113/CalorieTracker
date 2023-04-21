9e349f7a806755328637fe4acb5c0384da982b70
George <aptx113@gmail.com>
2023-04-21 17:54:02 +0800

    implement Welcome Screen

--- feature/onboarding/src/main/java/com/dante/calorietracker/feature/onboarding/welcome/WelcomeScreen.kt
+++ feature/onboarding/src/main/java/com/dante/calorietracker/feature/onboarding/welcome/WelcomeScreen.kt
@@ -1,20 +1,27 @@
 package com.dante.calorietracker.feature.onboarding.welcome
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.dante.calorietracker.core.ui.R
+import com.dante.calorietracker.core.ui.component.AppBackground
+import com.dante.calorietracker.core.ui.component.AppButton
 import com.dante.calorietracker.core.ui.component.ThemePreviews
 import com.dante.calorietracker.core.ui.theme.CalorieTrackerTheme
+import com.dante.calorietracker.core.ui.unit.LocalSpacing
 
 @Composable
 fun WelcomeScreen() {
+    val spacing = LocalSpacing.current
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -25,7 +32,10 @@ fun WelcomeScreen() {
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.displayLarge
         )
-        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_16)))
+        Spacer(modifier = Modifier.height(spacing.space16))
+        AppButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Text(text = stringResource(id = R.string.next))
+        }
     }
 }
 
@@ -33,6 +43,8 @@ fun WelcomeScreen() {
 @Composable
 fun WelcomeScreenPrev() {
     CalorieTrackerTheme {
-        WelcomeScreen()
+        AppBackground {
+            WelcomeScreen()
+        }
     }
 }
