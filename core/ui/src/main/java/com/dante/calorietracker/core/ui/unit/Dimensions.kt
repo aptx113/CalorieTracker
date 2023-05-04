f9943c5fb33acc62b4fb39372fcaabfaeed828dd
George <aptx113@gmail.com>
2023-05-04 15:59:41 +0800

    add CalorieTrackerSuggestionChip

--- core/ui/src/main/java/com/dante/calorietracker/core/ui/unit/Dimensions.kt
+++ core/ui/src/main/java/com/dante/calorietracker/core/ui/unit/Dimensions.kt
@@ -6,11 +6,13 @@ import androidx.compose.ui.unit.dp
 
 data class Dimensions(
     val space0: Dp = 0.dp,
+    val space2: Dp = 2.dp,
     val space4: Dp = 4.dp,
     val space8: Dp = 8.dp,
     val space16: Dp = 16.dp,
     val space32: Dp = 32.dp,
-    val space6d: Dp = 64.dp
+    val space64: Dp = 64.dp,
+    val space100: Dp = 100.dp
 )
 
 val LocalSpacing = staticCompositionLocalOf { Dimensions() }
